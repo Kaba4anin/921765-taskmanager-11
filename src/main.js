1e01629be51d159271c79730cead7bbecc30ac35
stasnislav <stas_kabanov@mail.ru>
2020-04-05 18:46:36 +0400

    Учебный проект: начинаем программировать - работа над ошибками

--- src/main.js
+++ src/main.js
@@ -373,15 +373,15 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
-const taskListElement = siteMainElement.querySelector('.board__tasks');
-const boardElement = siteMainElement.querySelector('.board');
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const boardElement = siteMainElement.querySelector(`.board`);
 
 render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
